a200b53f94953c20ccab3c016e31974070965c6c
Alvaro Tinoco <atinoco@hiberus.com>
2020-02-12 09:46:12 +0100

    chore: Added some dev tools to the stack

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest'],
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  rules: {
+    'no-console': 0,
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'import/named': 0,
+  },
+};
